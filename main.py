a378087898b464e20eb2e9d729fddd75782b031f
Soham Mhatre <106436641+Sohammhatre10@users.noreply.github.com>
2023-07-27 15:41:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from requests import get
 from key import api_key
 import time
-from key import password
-import mysql.connector
-conn = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password=password,
-    database='blockchain'
-)
-cursor = conn.cursor()
+import csv
+# from key import password
+# import mysql.connector
+# conn = mysql.connector.connect(
+#     host='localhost',
+#     user='root',
+#     password=password,
+#     database='blockchain'
+# )
+# cursor = conn.cursor()
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 header = {
     'X-CMC_PRO_API_KEY': api_key,
@@ -28,18 +29,29 @@ while True:
         if item['symbol'] == "BTC":
             timestamp = item['quote']['INR']['last_updated']
             price = item['quote']['INR']['price']
-            insert_query = "INSERT INTO btc (price) VALUES (%s)"
-            data_tuple = (price,)
-            try:
-                # Execute the query with the data_tuple
-                cursor.execute(insert_query, data_tuple)
+            #add data to csv file
+            with open('blockchain.csv', 'a', newline='') as csvfile:
+                fieldnames = ['Timestamp', 'Price']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-                # Commit the changes to the database
-                conn.commit()
+                # Check if the CSV file is empty and write headers if needed
+                if csvfile.tell() == 0:
+                    writer.writeheader()
 
-                print("Element inserted successfully.")
-            except mysql.connector.Error as error:
-                # Rollback the changes in case of an error
-                conn.rollback()
-                print(f"Error: {error}")
-        time.sleep(300)
+                writer.writerow({'Timestamp': timestamp, 'Price': price})
+    time.sleep(300)
+            # add data to sql_database
+            # insert_query = "INSERT INTO btc (price) VALUES (%s)"
+            # data_tuple = (price,)
+            # try:
+            #     # Execute the query with the data_tuple
+            #     cursor.execute(insert_query, data_tuple)
+            #
+            #     # Commit the changes to the database
+            #     conn.commit()
+            #
+            #     print("Element inserted successfully.")
+            # except mysql.connector.Error as error:
+            #     # Rollback the changes in case of an error
+            #     conn.rollback()
+            #     print(f"Error: {error}")
