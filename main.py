c38ffa64a6383e02dd0560a48ce4ed008caada9f
Soham Mhatre <106436641+Sohammhatre10@users.noreply.github.com>
2023-08-27 18:46:02 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -6,19 +6,18 @@ import time
 from pymongo import MongoClient
 import csv
 from key import username, password
-import matplotlib.pyplot as plt
 
 import shutil
 
+
 source_path = "C:/Users/soham/Downloads/modified_data.csv"
-
-# Specify the destination folder path
+#
+# # Specify the destination folder path
 destination_folder = "C:/Users/soham/PycharmProjects/Future_chain"
-
-# Construct the full destination path
+#
+# # Construct the full destination path
 destination_path = destination_folder + "/modified_data.csv"
 
-# Move the file to the destination folder
 shutil.move(source_path, destination_path)
 
 def update_data_cs(ticker_symbol):
@@ -31,19 +30,19 @@ def update_data_cs(ticker_symbol):
     updated_data = pd.concat([existing_data, coin_data])
     updated_data.to_csv(f"{ticker_symbol}.csv", index=False)
 
-def update_data(ticker_symbol):
 
+def update_data(ticker_symbol):
     today = datetime.now().date()
     coin_data = yf.download(f"{ticker_symbol}-INR", period='1d')
     coin_data['Date'] = today.strftime("%Y-%m-%d")
+    coin_data['Close'] = coin_data['Close']
     coin_data_dicts = coin_data.to_dict(orient='records')
 
     client = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@cluster0.ziyiwtr.mongodb.net/")
     db = client["Crypto_data"]
     collection = db[ticker_symbol]
-    collection_2 = db[f"{ticker_symbol}-PRED"]
 
-            # Insert the list of dictionaries into the collection
+    # Insert the list of dictionaries into the collection
     inserted_records = collection.insert_many(coin_data_dicts)
 
     print("Inserted IDs:", inserted_records.inserted_ids)
@@ -55,57 +54,49 @@ def update_data(ticker_symbol):
 def pred(ticker_symbol):
     csv_file_path = "modified_data.csv"
     today = datetime.now().date()
-    coin_data = yf.download(f"{ticker_symbol}-INR", period='1d')
+    coin_data = yf.download(f"{ticker_symbol}-INR", start=today - timedelta(days=365), end=today)
     selected_value = 0
     coin_data_1 = pd.read_csv(csv_file_path)
     coin_data["Date"] = datetime.now().date()
-    # print(coin_data)
-    original_date = coin_data["Date"].iloc[0]  # Get the first date value as a single datetime object
-
+    original_date = coin_data["Date"].iloc[0]
     new_d = original_date + timedelta(days=1)
     new_date = new_d.strftime("%Y-%m-%d")
-    #     if ticker_symbol in coin_data_1.columns:
     selected_value = coin_data_1.loc[coin_data_1['Date'] == new_date, ticker_symbol].values
-    #     else:
-    #         print("BTC column not found in the DataFrame.")
     coin_update = pd.DataFrame(coin_data)
-    coin_update["Close"] = float(coin_data["Close"].iloc[0]) * selected_value
-    print(coin_update["Close"].iloc[0])
-
     client = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@cluster0.ziyiwtr.mongodb.net/")
     db = client["Crypto_data"]
-    collection_2 = db[f"{ticker_symbol}-PRED-1"]
-    data_dict = {
-        "Date": new_d.strftime("%Y-%m-%d"),
-        "Close": coin_update["Close"].iloc[0]
-    }
-    inserted_record = collection_2.insert_one(data_dict)  # Use insert_one for a single document
-    print("Inserted ID:", inserted_record.inserted_id)
-    coin_data_graph = pd.Dataframe([coin_data['Date'], coin_data['Close'])
-    coin_data_graph.append(data_dict)
-    k = list(range(len(coin_data_graph))
-    plt.plot(k,coin_data_graph.Close,color="red")
-    plt.show()
-    plt.title(f"{ticker_symbol} with respect to time")
+    collection_2 = db[f"{ticker_symbol}-PRED"]
+    coin_update["Close"] = coin_data["Close"] + (coin_data["Close"].iloc[len(coin_data)-1] - coin_data["Close"].iloc[0])
+    coin_update["Close"] = coin_update["Close"] * selected_value
+    index_values = list(range(len(coin_update["Close"])))
+    coin_update["Index"] = index_values
+    # print(coin_update)
+    for idx,row in coin_update.iterrows():
+            data_dict = {
+                "Date": row["Date"] + timedelta(days=(row["Index"])),
+                "Close": coin_update["Close"].iloc[(row["Index"])]
+            }
+            # Convert datetime.date to datetime.datetime
+            data_dict["Date"] = datetime.combine(data_dict["Date"], datetime.min.time())
+
+            inserted_record = collection_2.insert_one(data_dict)
+            print("Inserted ID:", inserted_record.inserted_id)
+
+
 def delete_data(ticker_symbol):
     client = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@cluster0.ziyiwtr.mongodb.net/")
     db = client["Crypto_data"]
     collection = db[f"{ticker_symbol}-PRED"]
     last_inserted_document = collection.find_one(sort=[("_id", pymongo.DESCENDING)])
-
-    if last_inserted_document:
-        # Delete the last inserted document
-        result = collection.delete_one({"_id": last_inserted_document["_id"]})
-
-        if result.deleted_count == 1:
-            print("Last inserted document deleted successfully.")
-        else:
-            print("Failed to delete the last inserted document.")
-    else:
-        print("No documents found in the collection.")
+    result = collection.delete_many({})
 
 if __name__ == "__main__":
     while True:
+        delete_data('BTC')
+        delete_data('DOGE')
+        delete_data('XRP')
+        delete_data('LTC')
+        delete_data('ETH')
         update_data_cs('BTC')
         update_data_cs('DOGE')
         update_data_cs('XRP')
@@ -122,8 +113,4 @@ if __name__ == "__main__":
         pred('LTC')
         pred('ETH')
         time.sleep(86400)
-        delete_data('BTC')
-        delete_data('DOGE')
-        delete_data('XRP')
-        delete_data('LTC')
-        delete_data('ETH')
+
